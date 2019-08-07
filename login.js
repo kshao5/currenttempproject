9055054af77c566356050ef9c7c80641ecb22e03
Kedong Shao <kshao5@illinois.edu>
2019-08-06 22:21:49 -0400

    submit changes

--- login.js
+++ login.js
@@ -1,7 +1,10 @@
-This is login.js, hahaha
+his is login.js, hahaha
 this is another login.js lol
 aaaa
 bbbb
 cccc
 there is conflict
 there is another conflict
+
+There is third conflict
+There is fourth conflict
